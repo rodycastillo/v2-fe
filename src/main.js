675507a7fc74d67d0bb355edfd1b0aa336845ab0
Carlito <rody.castillo.tacuri@gmail.com>
2021-11-04 23:31:10 -0500

    Add new view notas

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
+
+axios.defaults.baseURL = 'http://localhost:2021/api';
 
 Vue.config.productionTip = false
 
---
+++ src/views/Notas.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col">Descripción</th>
+          <th scope="col">Fecha</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in notas" :key="index">
+          <th scope="row">{{ item._id }}</th>
+          <td>{{ item.nombre }}</td>
+          <td>{{ item.descripcion }}</td>
+          <td>{{ item.date }}</td>
+          <td>
+            <b-button
+              class="btn-warning btn-sm mx-2"
+              @click="activarEdicion(item._id)"
+              >Actualizar</b-button
+            >
+            <b-button
+              class="btn-danger btn-sm mx-2"
+              @click="eliminarNota(item._id)"
+              >Eliminar</b-button
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notas: [],
+    };
+  },
+  created() {
+    this.listarNotas();
+  },
+  methods: {
+    listarNotas() {
+      this.axios
+        .get("notas")
+        .then((response) => {
+          // console.log(response.data)
+          this.notas = response.data;
+        })
+        .catch((e) => {
+          console.log("error" + e);
+        });
+    },
+  },
+};
+</script>
